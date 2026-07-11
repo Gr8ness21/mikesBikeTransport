ee74497aeff19508812ccd44f691a3479ac46c5d
Gr8ness21 <pmleechapman@gmail.com>
2026-07-11 10:27:46 -0400

    added modal logic and css styling for simple modal

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-// Modal on page load
-/*
-Leave some room for a gentle pop-up 
-*/
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("closeModal");
+
+const navItems = [
+    document.getElementById("about"),
+    document.getElementById("contact"),
+    document.getElementById("pickup")
+];
+
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        modal.classList.remove("hidden");
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
+
+modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.classList.add("hidden");
+    }
+});
